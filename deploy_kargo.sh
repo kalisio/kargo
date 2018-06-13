d92b2dd29e3bcac3cfee3496425a22a24ff98497
Christophe Nouguier <christophe.nouguier@free.fr>
2018-06-13 16:27:44 +0200

    Updated to fit Kaabah requirements

---
+++ deploy_kargo.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -a
+. ./.env
+set +a
+
+EXISTING_DOCKER_NETWORK=`docker network ls | grep $DOCKER_NETWORK`
+if [ "$EXISTING_DOCKER_NETWORK" = "" ]; then
+  docker network create -d overlay --attachable $DOCKER_NETWORK
+fi
+docker stack deploy -c tileservergl.yml -c tileservergl.swarm.yml -c mapproxy.yml -c mapproxy.swarm.yml kargo
+
