1852eb6ea4242a9ab8170f5a2cce6231c6c9259a
Christophe Nouguier <christophe.nouguier@free.fr>
2018-06-13 23:27:25 +0200

    Removed wmts tests Removed basic auth on mapproxy

--- deploy-kargo.sh
+++ deploy-kargo.sh
@@ -7,5 +7,5 @@ EXISTING_DOCKER_NETWORK=`docker network ls | grep $DOCKER_NETWORK`
 if [ "$EXISTING_DOCKER_NETWORK" = "" ]; then
   docker network create -d overlay --attachable $DOCKER_NETWORK
 fi
-docker stack deploy -c tileservergl.yml -c tileservergl.swarm.yml -c mapproxy.yml -c mapproxy.swarm.yml -c wmts.yml kargo
+docker stack deploy -c tileservergl.yml -c tileservergl.swarm.yml -c mapproxy.yml -c mapproxy.swarm.yml kargo
 
