300107df54e57ff8845b4b21902e15c9ada327cd
Christophe Nouguier <christophe.nouguier@free.fr>
2018-06-28 16:00:19 +0200

    Updated deploy method

--- deploy-kargo.sh
+++ deploy-kargo.sh
@@ -3,9 +3,17 @@ set -a
 . ./.env
 set +a
 
+# Check whether the network exist or not.
 EXISTING_DOCKER_NETWORK=`docker network ls | grep $DOCKER_NETWORK`
 if [ "$EXISTING_DOCKER_NETWORK" = "" ]; then
   docker network create -d overlay --attachable $DOCKER_NETWORK
 fi
-docker stack deploy -c tileservergl.yml -c tileservergl.swarm.yml -c mapproxy.yml -c mapproxy.swarm.yml -c vigicrues.yml -c vigicrues.swarm.yml kargo
+
+# Deploy the required services only
+SERVICES_TO_DEPLOY=
+for SERVICE in $SERVICES; do
+  SERVICES_TO_DEPLOY="$SERVICES_TO_DEPLOY -c $SERVICE.yml -c $SERVICE.swarm.yml"
+done
+docker stack deploy $SERVICES_TO_DEPLOY kargo
+
 
