a80e80257241ca2ff10a2a9c157243f617b38dbe
Christophe Nouguier <christophe.nouguier@free.fr>
2018-09-19 19:04:32 +0200

    WIP #9 [skip ci]

--- scripts/build-weacast.sh
+++ scripts/build-weacast.sh
@@ -4,7 +4,7 @@ WEACAST_MODELS=$1
 # Build the images for the different models
 for WEACAST_MODEL in $WEACAST_MODELS; do
   docker build https://github.com/weacast/weacast-loader.git -f dockerfile.$WEACAST_MODEL -t weacast-$WEACAST_MODEL
-  docker tag weacast-$WEACAST_MODEL $MANAGER_IP:5000/weacast-$WEACAST_MODEL:latest
-  docker push $MANAGER_IP:5000/weacast-$WEACAST_MODEL:latest
+  docker tag weacast-$WEACAST_MODEL localhost:5000/weacast-$WEACAST_MODEL:latest
+  docker push localhost:5000/weacast-$WEACAST_MODEL:latest
   sed -e "s/WEACAST_MODEL/$WEACAST_MODEL/g" scripts/weacast-loader.tpl > weacast-$WEACAST_MODEL.yml
 done
