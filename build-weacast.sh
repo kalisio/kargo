3f6770dc662aab2fc179983b16582d544c3a78c5
Ubuntu <ubuntu@ip-172-31-40-21.eu-central-1.compute.internal>
2018-09-20 14:45:02 +0000

    fixed sh mode

--- build-weacast.sh
+++ build-weacast.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-WEACAST_MODELS=$1
+set -a
+. ./.env
+set +a
 
 # Build the images for the different models
-for WEACAST_MODEL in $WEACAST_MODELS; do
+for WEACAST_MODEL in $WEACAST_STACK; do
   docker build https://github.com/weacast/weacast-loader.git -f dockerfile.$WEACAST_MODEL -t weacast-$WEACAST_MODEL
   docker tag weacast-$WEACAST_MODEL localhost:5000/weacast-$WEACAST_MODEL:latest
   docker push localhost:5000/weacast-$WEACAST_MODEL:latest
