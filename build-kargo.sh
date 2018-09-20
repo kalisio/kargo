01d023bb3fbd2e9fcbabfe368a021e0c75e43385
Christophe Nouguier <christophe.nouguier@free.fr>
2018-09-20 20:00:37 +0200

    Fixed #14

--- build-kargo.sh
+++ build-kargo.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-set -a
-. ./.env
-set +a
-
-if [ "$WEACAST_MODELS" != "" ]; then
-  sh scripts/build-weacast.sh $WEACAST_MODELS 
-fi
+./build-weacast.sh
--- deploy-kargo.sh
+++ deploy-kargo.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sh ./deploy-dbs.sh
-sh ./deploy-apps.sh
-sh ./deploy-weacast.sh
-sh ./deploy-krawlers.sh
+./deploy-stack.sh dbs
+./deploy-stack.sh apps
+./deploy-stack.sh weacast
+./deploy-stack.sh krawlers
