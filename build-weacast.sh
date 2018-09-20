3d549a45d3dea4b77f93a8a2983a9010c76739f1
Ubuntu <ubuntu@ip-172-31-40-21.eu-central-1.compute.internal>
2018-09-20 15:17:49 +0000

    Updated build script

--- build-weacast.sh
+++ build-weacast.sh
@@ -8,5 +8,5 @@ for WEACAST_MODEL in $WEACAST_STACK; do
   docker build https://github.com/weacast/weacast-loader.git -f dockerfile.$WEACAST_MODEL -t weacast-$WEACAST_MODEL
   docker tag weacast-$WEACAST_MODEL localhost:5000/weacast-$WEACAST_MODEL:latest
   docker push localhost:5000/weacast-$WEACAST_MODEL:latest
-  sed -e "s/WEACAST_MODEL/$WEACAST_MODEL/g" deploy/weacast-loader.tpl > deploy/weacast-$WEACAST_MODEL.yml
+  sed -e "s/WEACAST_MODEL/$WEACAST_MODEL/g" deploy/weacast-loader.tpl > deploy/$WEACAST_MODEL.yml
 done
