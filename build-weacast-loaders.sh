251d4bad8088cc0af0d2944b329b688f2c6815d5
Ubuntu <ubuntu@ip-172-31-40-21.eu-central-1.compute.internal>
2018-09-24 12:35:36 +0000

    WIP #16

--- build-weacast-loaders.sh
+++ build-weacast-loaders.sh
@@ -6,6 +6,6 @@ set +a
 # Build the images for the different models
 for WEACAST_MODEL in $WEACAST_MODELS; do
   docker build https://github.com/weacast/weacast-loader.git -f dockerfile.$WEACAST_MODEL -t $WEACAST_MODEL-loader
-  docker tag $WEACAST_MODEL-loader localhost:5000/weacast-$WEACAST_MODEL:latest
+  docker tag $WEACAST_MODEL-loader localhost:5000/$WEACAST_MODEL-loader:latest
   docker push localhost:5000/$WEACAST_MODEL-loader:latest
 done
