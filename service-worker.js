8cf30876ee9fc02c4bf6a89004620352b8e8fcdf
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-11-07 17:33:50 +0000

    Deploy kalisio/kargo to github.com/kalisio/kargo.git:gh-pages

--- service-worker.js
+++ service-worker.js
@@ -21,7 +21,7 @@ importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.5.0/workbox
 self.__precacheManifest = [
   {
     "url": "404.html",
-    "revision": "43c98bbc77af37525c15c5a1dd309422"
+    "revision": "6b5abd9db8297f0d70bddd8d6d32a2bb"
   },
   {
     "url": "assets/css/1.styles.36c1a3fe.css",
@@ -32,7 +32,7 @@ self.__precacheManifest = [
     "revision": "df7c35ec8029dbbde0735a45f875eaf7"
   },
   {
-    "url": "assets/css/styles.3e87aa42.css",
+    "url": "assets/css/styles.b9e7ddd1.css",
     "revision": "c1703aa2a4d7eef65645a2158d133a03"
   },
   {
@@ -92,40 +92,40 @@ self.__precacheManifest = [
     "revision": "99be267dc53af274527dd3e8770d6a50"
   },
   {
-    "url": "assets/js/app.3e87aa42.js",
-    "revision": "fd26f87e420b075f617ae90b65cb80c4"
+    "url": "assets/js/app.b9e7ddd1.js",
+    "revision": "8ed5659aabac39f1af8d275e16c3c0be"
   },
   {
     "url": "CONTRIBUTING.html",
-    "revision": "89a130a8eef552d60e03fe8468a1314b"
+    "revision": "501c8c63ffddb45b19db175ea8f171a6"
   },
   {
     "url": "how-does-it-work/index.html",
-    "revision": "7c8780f825e1b24ad181f02fd87d9118"
+    "revision": "5c9ce25eb2d1eec7c9507bb7a23a59a0"
   },
   {
     "url": "how-to-use-it/getting-started.html",
-    "revision": "026056b83f87d50ff13043596bac606a"
+    "revision": "46a51b1dfc72ee6a47100f7c0b938af0"
   },
   {
     "url": "how-to-use-it/tips.html",
-    "revision": "aaf7726cd1c0a967804e0f0237d19b75"
+    "revision": "d78f809e43bbe100cc7b8c2308c7d4e7"
   },
   {
     "url": "index.html",
-    "revision": "27ea204111c241c051b7a3e89cc4dbf1"
+    "revision": "6f5ea6b734609ad0eec816cab0a6a224"
   },
   {
     "url": "LICENSE.html",
-    "revision": "5da94375792e6534b99c9f7ffa585c33"
+    "revision": "25f2b3e2e110911ceb4d3d2cbf65d14c"
   },
   {
     "url": "license/index.html",
-    "revision": "3f1b95332e9bfaaf2ae5c77b64ea22fc"
+    "revision": "d7f2ec522baffcad0a374a17ee7f47f2"
   },
   {
     "url": "what-is-it/index.html",
-    "revision": "37cec081969ef188e58cd9d06facea06"
+    "revision": "30b6d5246f951bcd36005a92aeb3d7b9"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
