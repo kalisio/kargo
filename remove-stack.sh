ad7a086570975b2e4b0c0ef9d3dd6f36d2333f03
Christophe Nouguier <christophe.nouguier@free.fr>
2018-11-25 13:59:30 +0100

    Fixed scripts issues

--- remove-stack.sh
+++ remove-stack.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [[ ! "$1" =~ ^(apps|dbs|weacast|vigicrues|jobs)$ ]]; then
+if [[ ! "$1" =~ ^(apps|dbs|weacast|vigicrues|jobs|seeder)$ ]]; then
   echo "usage: remove-stack.sh <apps|dbs|weacast|vigicrues|jobs|seeder>"
   exit 1
 fi
