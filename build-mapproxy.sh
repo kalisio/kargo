b2e42c66b37e79e74020f808783c1e2cae8b63de
Ubuntu <ubuntu@ip-172-31-35-8.eu-central-1.compute.internal>
2019-01-08 22:24:51 +0000

    WIP #30

--- build-mapproxy.sh
+++ build-mapproxy.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-docker build ./build/mapproxy -f dockerfile -t mapproxy
-docker tag mapproxy 127.0.0.1:5000/mapproxy:1.10.4-alpine
-docker push 127.0.0.1:5000/mapproxy:1.10.4-alpine
+
+cd .kargo
+set -a
+. ./.env
+set +a
+cd ..
+
+cd build/mapproxy
+docker build --build-arg MAPPROXY_VERSION=$MAPPROXY_TAG -f dockerfile -t mapproxy:$MAPPROXY_TAG .
+docker tag mapproxy:$MAPPROXY_TAG 127.0.0.1:5000/mapproxy:$MAPPROXY_TAG
+docker push 127.0.0.1:5000/mapproxy:$MAPPROXY_TAG
 
