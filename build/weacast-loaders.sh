d7fc5a93ae883471a43f7d36a659bd2edab00311
Ubuntu <ubuntu@ip-172-31-35-8.eu-central-1.compute.internal>
2019-01-09 14:53:30 +0000

    WIP #34

--- build/weacast-loaders.sh
+++ build/weacast-loaders.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 for WEACAST_MODEL in $WEACAST_MODELS; do
-  TAG_NAME=${WEACAST_MODEL}_LOADER_TAG
-  TAG=${!TAG_NAME}
-  docker build https://github.com/weacast/weacast-loader.git -f dockerfile.$WEACAST_MODEL -t $WEACAST_MODEL-loader:$TAG
-  docker tag $WEACAST_MODEL-loader:$TAG 127.0.0.1:5000/$WEACAST_MODEL-loader:$TAG
-  docker push 127.0.0.1:5000/$WEACAST_MODEL-loader:$TAG
+  WEACAST_LOADER_TAG_NAME=${WEACAST_MODEL^^}_LOADER_TAG
+  WEACAST_LOADER_TAG_NAME=${WEACAST_LOADER_TAG_NAME//-/_}
+  WEACAST_LOADER_TAG=${!WEACAST_LOADER_TAG_NAME}
+  GIT_URL=https://github.com/weacast/weacast-loader.git
+  GIT_BRANCH=$WEACAST_LOADER_TAG
+  if [ "$GIT_BRANCH" != "latest" ]; then
+    GIT_URL=$GIT_URL#$GIT_BRANCH
+  fi
+  docker build $GIT_URL -f dockerfile.$WEACAST_MODEL -t $WEACAST_MODEL-loader:$WEACAST_LOADER_TAG
+  docker tag $WEACAST_MODEL-loader:$WEACAST_LOADER_TAG 127.0.0.1:5000/$WEACAST_MODEL-loader:$WEACAST_LOADER_TAG
+  docker push 127.0.0.1:5000/$WEACAST_MODEL-loader:$WEACAST_LOADER_TAG
 done
