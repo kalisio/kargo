09584c76378b3e3e8eb1e138242b981749531818
Ubuntu <ubuntu@ip-172-31-35-8.eu-central-1.compute.internal>
2019-01-09 16:27:12 +0000

    WIP #30

--- build/geoserver.sh
+++ build/geoserver.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
-cd .kargo
-set -a
-. ./.kargo/.env
-set +a
-cd ..
-
-cd build/geoserver
-docker build --build-arg VERSION=$TAG_VERSION --build-arg "PLUGINS=$GEOSERVER_PLUGINS" -f dockerfile -t geoserver:$TAG_VERSION .
-docker tag geoserver:$TAG_VERSION 127.0.0.1:5000/geoserver:$TAG_VERSION
-docker push 127.0.0.1:5000/geoserver:$TAG_VERSION
+cd geoserver
+docker build --build-arg VERSION=$GEOSERVER_TAG  --build-arg "PLUGINS=$GEOSERVER_PLUGINS" -f dockerfile -t geoserver:$GEOSERVER_TAG .
+docker tag geoserver:$GEOSERVER_TAG 127.0.0.1:5000/geoserver:$GEOSERVER_TAG
+docker push 127.0.0.1:5000/geoserver:$GEOSERVER_TAG
 
