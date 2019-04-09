1de790dcb609f3d8b5bfe8a27b49a09a7238dff4
Christophe Nouguier <christophe.nouguier@free.fr>
2019-04-09 10:54:25 +0200

    wip: #49

---
+++ build/kano.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd kano
+docker build --build-arg TAG=$KANO_TAG -f dockerfile -t kano:$KANO_TAG .
+docker tag kano:$KANO_TAG 127.0.0.1:5000/kano:$KANO_TAG
+docker push 127.0.0.1:5000/kano:$KANO_TAG
