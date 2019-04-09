ac0d4228aa08ca7bf0665e24ce06278ad317245f
Christophe Nouguier <christophe.nouguier@free.fr>
2019-04-09 13:50:15 +0200

    chore: handle config directory when building kano

--- build/kano.sh
+++ build/kano.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 cd kano
+# Create the archives to be merged 
 tar cvf src.tgz src
+tar cvf config.tgz config
+# Build the image
 docker build --build-arg TAG=$KANO_TAG -f dockerfile -t kano:$KANO_TAG .
 docker tag kano:$KANO_TAG 127.0.0.1:5000/kano:$KANO_TAG
 docker push 127.0.0.1:5000/kano:$KANO_TAG
