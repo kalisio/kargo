10da411e0675756c9846a7fbf5c6c9288d50cebd
Ubuntu <ubuntu@ip-172-31-35-8.eu-central-1.compute.internal>
2019-04-09 11:01:44 +0000

    feat: Create a build script for Kano (close #49)

--- build/kano.sh
+++ build/kano.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd kano
+tar cvf src.tgz src
 docker build --build-arg TAG=$KANO_TAG -f dockerfile -t kano:$KANO_TAG .
 docker tag kano:$KANO_TAG 127.0.0.1:5000/kano:$KANO_TAG
 docker push 127.0.0.1:5000/kano:$KANO_TAG
