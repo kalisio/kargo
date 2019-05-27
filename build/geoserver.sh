0beb70a9edab2f6c1b7c6325b035a705762d1ec5
Christophe Nouguier <christophe.nouguier@free.fr>
2019-05-27 09:31:33 +0200

    wip: Provide a subcommand to update the services  (#63)

--- build/geoserver.sh
+++ build/geoserver.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd geoserver
-docker build --build-arg VERSION=$GEOSERVER_TAG  --build-arg "PLUGINS=$GEOSERVER_PLUGINS" -f dockerfile -t geoserver:$GEOSERVER_TAG .
+docker build --build-arg VERSION=$GEOSERVER_TAG --build-arg "PLUGINS=$GEOSERVER_PLUGINS" -f dockerfile -t geoserver:$GEOSERVER_TAG .
 docker tag geoserver:$GEOSERVER_TAG 127.0.0.1:5000/geoserver:$GEOSERVER_TAG
 docker push 127.0.0.1:5000/geoserver:$GEOSERVER_TAG
 
