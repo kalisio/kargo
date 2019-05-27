84d56d9b0f2384fd386acdbb23408af17ce49565
Christophe Nouguier <christophe.nouguier@free.fr>
2019-05-27 16:35:45 +0200

    wip: Attempt to pull a newer image when building the services (#62)

--- build/mapproxy.sh
+++ build/mapproxy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd mapproxy
-docker build --build-arg MAPPROXY_VERSION=$MAPPROXY_TAG -f dockerfile -t mapproxy:$MAPPROXY_TAG .
+docker build --pull --build-arg MAPPROXY_VERSION=$MAPPROXY_TAG -f dockerfile -t mapproxy:$MAPPROXY_TAG .
 docker tag mapproxy:$MAPPROXY_TAG 127.0.0.1:5000/mapproxy:$MAPPROXY_TAG
 docker push 127.0.0.1:5000/mapproxy:$MAPPROXY_TAG
 
--- build/mapserver.sh
+++ build/mapserver.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd mapserver
-docker build --build-arg TAG=$MAPSERVER_TAG -f dockerfile -t mapserver:$MAPSERVER_TAG .
+docker build --pull --build-arg TAG=$MAPSERVER_TAG -f dockerfile -t mapserver:$MAPSERVER_TAG .
 docker tag mapserver:$MAPSERVER_TAG 127.0.0.1:5000/mapserver:$MAPSERVER_TAG
 docker push 127.0.0.1:5000/mapserver:$MAPSERVER_TAG
