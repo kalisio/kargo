79f031d34b9fc2c54cb98488b06e21098349c9ad
Ubuntu <ubuntu@ip-172-31-38-130.eu-central-1.compute.internal>
2019-06-18 12:49:15 +0000

    wip: Test the deployment of Hyrax #45

--- build/hyrax.sh
+++ build/hyrax.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-# OLFS
-OLFS_GIT_URL=https://github.com/OPENDAP/hyrax-docker/hyrax-${HYRAX_TAG}/hyrax/olfs/Dockerfile
+echo Building HYRAX $HYRAX_TAG
 
-docker build --pull --build-arg TAG=$HYRAX_TAG -f dockerfile -t hyrax-olfs:$HYRAX_TAG $OLFS_GIT_URL
+GIT_URL=https://github.com/OPENDAP/hyrax-docker.git
+
+# OLFS
+docker build --pull --build-arg TAG=$HYRAX_TAG -t hyrax-olfs:$HYRAX_TAG $GIT_URL#:hyrax-${HYRAX_TAG}/olfs
 docker tag hyrax-olfs:$HYRAX_TAG 127.0.0.1:5000/hyrax-olfs:$HYRAX_TAG
 docker push 127.0.0.1:5000/hyrax-olfs:$HYRAX_TAG
 
 # BES
-BES_GIT_URL=https://github.com/OPENDAP/hyrax-docker/hyrax-${HYRAX_TAG}/hyrax/bes/Dockerfile
-
-docker build --pull --build-arg TAG=$HYRAX_TAG -f dockerfile -t hyrax-bes:$HYRAX_TAG $BES_GIT_URL
+docker build --pull --build-arg TAG=$HYRAX_TAG -t hyrax-bes:$HYRAX_TAG $GIT_URL#:hyrax-${HYRAX_TAG}/bes
 docker tag hyrax-olfs:$HYRAX_TAG 127.0.0.1:5000/hyrax-bes:$HYRAX_TAG
 docker push 127.0.0.1:5000/hyrax-bes:$HYRAX_TAG
