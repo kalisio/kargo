9fa62b1e383c1302eeda52a12a52f799e7b58193
Luc Claustres <luc.claustres@orange.fr>
2019-10-17 13:18:47 +0200

    Added weacast build scripts

---
+++ build/weacast-api.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd weacast-api
+# Create the archives to be merged 
+tar cvf src.tgz src
+tar cvf config.tgz config
+# Build the image
+docker build --pull --build-arg TAG=$WEACAST_TAG -f dockerfile -t localhost:5000:$WEACAST_TAG .
+docker push localhost:5000/weacast-api:$WEACAST_TAG
