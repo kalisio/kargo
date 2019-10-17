7e177f9325d5472e591d7737e684a386d4f48f70
Luc Claustres <luc.claustres@orange.fr>
2019-10-17 16:10:57 +0200

    Fixed build path for kano/weacast config

--- build/kano.sh
+++ build/kano.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd kano
 # Create the archive to be merged 
-tar cvf kano.tgz ../configs/kano
+tar cvf kano.tgz ../../configs/kano
 # Build the image
 docker build --pull --build-arg TAG=$KANO_TAG -f dockerfile -t localhost:5000/kano:$KANO_TAG .
 docker push localhost:5000/kano:$KANO_TAG
--- build/weacast-api.sh
+++ build/weacast-api.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd weacast-api
 # Create the archive to be merged 
-tar cvf weacast.tgz ../configs/weacast
+tar cvf weacast.tgz ../../configs/weacast
 # Build the image
 docker build --pull --build-arg TAG=$WEACAST_TAG -f dockerfile -t localhost:5000/kano:$WEACAST_TAG .
 docker push localhost:5000/weacast-api:$WEACAST_TAG
