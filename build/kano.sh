0c5490f378997c0dd9d82ebeb9e6cc78390b20e4
Luc Claustres <luc.claustres@orange.fr>
2019-12-12 09:03:51 +0100

    Fixed https://github.com/kalisio/kargo/issues/111

--- build/kano.sh
+++ build/kano.sh
@@ -3,5 +3,5 @@ cd kano
 # Create the archive to be merged 
 tar cvf kano.tgz -C ../../configs/kano .
 # Build the image
-docker build --pull --force-rm --build-arg TAG=$KANO_TAG --build-arg SUBDOMAIN -f dockerfile -t localhost:5000/kano:$KANO_TAG .
+docker build --pull --force-rm --build-arg TAG=$KANO_TAG --build-arg SUBDOMAIN --build-arg NODE_APP_INSTANCE --build-arg NODE_ENV -f dockerfile -t localhost:5000/kano:$KANO_TAG .
 docker push localhost:5000/kano:$KANO_TAG
