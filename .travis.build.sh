567ddf51e694445d4b07a5d4cf8626d287bd363f
Christophe Nouguier <christophe.nouguier@free.fr>
2020-01-06 17:18:23 +0100

    wip: Provide a ready to deploy image of the gateway #119

--- .travis.build.sh
+++ .travis.build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Build express-gateway
-EXPRESS_GATEWAY_VERSION=1.6.9
-cd ./build/express-gateway
-docker build --build-arg TAG=$EXPRESS_GATEWAY_VERSION -f dockerfile -t kalisio/express-gateway:$EXPRESS_GATEWAY_VERSION .
+EXPRESS_GATEWAY_VERSION=1.16.9
+cd build/express-gateway
+docker build --force-rm --build-arg TAG=$EXPRESS_GATEWAY_VERSION -f dockerfile -t kalisio/express-gateway:$EXPRESS_GATEWAY_VERSION .
 RESULT_CODE=$?
 if [ $RESULT_CODE -ne 0 ]; then
   echo "express-gateway generation failed [error: $RESULT_CODE]"
