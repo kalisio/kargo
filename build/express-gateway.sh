88d41f667ce71f8bf1f7c2fd241e9d8a182911aa
cnouguier <christophe.nouguier@free.fr>
2020-01-06 11:20:26 +0000

    build: fixed express-gateway image name

--- build/express-gateway.sh
+++ build/express-gateway.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd express-gateway
-docker build --pull --force-rm --build-arg TAG=$EXPRESS_GATEWAY_TAG -f dockerfile -t localhost:5000/express-gateway:$EXPRESS_GATEWAY_TAG .
-docker push localhost:5000/express-gateway:$EXPRESS_GATEWAY_TAG
+docker build --pull --force-rm --build-arg TAG=$EXPRESS_GATEWAY_TAG -f dockerfile -t $EXPRESS_GATEWAY_IMAGE:$EXPRESS_GATEWAY_TAG .
+docker push $EXPRESS_GATEWAY_IMAGE:$EXPRESS_GATEWAY_TAG
