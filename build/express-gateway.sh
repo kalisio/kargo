ea4aabfb0dd8defb8ba2778bd4efe53c38f7ecdf
Christophe Nouguier <christophe.nouguier@free.fr>
2020-05-09 12:39:02 +0200

    wip: Provide a tool to perform command line processing #156

--- build/express-gateway.sh
+++ build/express-gateway.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd express-gateway
-docker build --pull --force-rm --build-arg TAG=$EXPRESS_GATEWAY_TAG -f dockerfile -t $EXPRESS_GATEWAY_IMAGE:$EXPRESS_GATEWAY_TAG .
+docker build --pull --force-rm --build-arg VERSION=$EXPRESS_GATEWAY_TAG -f dockerfile -t $EXPRESS_GATEWAY_IMAGE:$EXPRESS_GATEWAY_TAG .
 docker push $EXPRESS_GATEWAY_IMAGE:$EXPRESS_GATEWAY_TAG
