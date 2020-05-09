1d868c5baee7009b5d7d10499bb9a7974fbc739a
Christophe Nouguier <christophe.nouguier@free.fr>
2020-05-09 12:30:21 +0200

    wip: Provide a tool to perform command line processing #156

--- .travis.build.sh
+++ .travis.build.sh
@@ -1,61 +1,42 @@
 #!/bin/bash
 
-# Build express-gateway
-if [ $1 == "express-gateway" ]; then
-  EXPRESS_GATEWAY_VERSION=1.16.9
-  pushd build/express-gateway
-  docker build --force-rm --build-arg TAG=$EXPRESS_GATEWAY_VERSION -f dockerfile -t kalisio/express-gateway:$EXPRESS_GATEWAY_VERSION .
+build_and_push()
+{
+  TOOL=$1
+  TAG=$2
+  pushd build/$TOOL
+  docker build --force-rm --build-arg VERSION=$TAG -f dockerfile -t kalisio/$TOOL:$TAG .
   RESULT_CODE=$?
   if [ $RESULT_CODE -ne 0 ]; then
-    echo "express-gateway generation failed [error: $RESULT_CODE]"
+    echo "$TOOL generation failed [error: $RESULT_CODE]"
     exit 1
   fi
   docker login -u="$DOCKER_USER" -p="$DOCKER_PASSWORD"
-  docker push kalisio/express-gateway:$EXPRESS_GATEWAY_VERSION
+  docker push kalisio/$TOOL:$TAG
   popd
+}
+
+# Build express-gateway
+if [ $1 == "express-gateway" ]; then
+  build_and_push express-gateway 1.16.9
 fi
 
 # Build Maputnik
 if [ $1 == "maputnik" ]; then
-  MAPUTNIK_VERSION=1.6.1
-  pushd build/maputnik
-  docker build --force-rm --build-arg VERSION=v$MAPUTNIK_VERSION -f dockerfile -t kalisio/maputnik:$MAPUTNIK_VERSION .
-  RESULT_CODE=$?
-  if [ $RESULT_CODE -ne 0 ]; then
-    echo "Maputnik generation failed [error: $RESULT_CODE]"
-    exit 1
-  fi
-  docker login -u="$DOCKER_USER" -p="$DOCKER_PASSWORD"
-  docker push kalisio/maputnik:$MAPUTNIK_VERSION
-  popd
+  build_and_push maputnik 1.6.1
 fi
 
 # Build MapProxy
 if [ $1 == "mapproxy" ]; then
-  MAPPROXY_VERSION=1.12.0
-  pushd build/mapproxy
-  docker build --force-rm --build-arg VERSION=$MAPPROXY_VERSION -f dockerfile -t kalisio/mapproxy:$MAPPROXY_VERSION .
-  RESULT_CODE=$?
-  if [ $RESULT_CODE -ne 0 ]; then
-    echo "MapProxy generation failed [error: $RESULT_CODE]"
-    exit 1
-  fi
-  docker login -u="$DOCKER_USER" -p="$DOCKER_PASSWORD"
-  docker push kalisio/mapproxy:$MAPPROXY_VERSION
-  popd
+  build_and_push mapproxy 1.12.0
 fi
 
 # Build Artillery
 if [ $1 == "artillery" ]; then
-  ARTILLERY_VERSION=1.6.0
-  pushd build/artillery
-  docker build --force-rm --build-arg VERSION=$ARTILLERY_VERSION -f dockerfile -t kalisio/artillery:$ARTILLERY_VERSION .
-  RESULT_CODE=$?
-  if [ $RESULT_CODE -ne 0 ]; then
-    echo "Artillery generation failed [error: $RESULT_CODE]"
-    exit 1
-  fi
-  docker login -u="$DOCKER_USER" -p="$DOCKER_PASSWORD"
-  docker push kalisio/artillery:$ARTILLERY_VERSION
-  popd
+  build_and_push artillery 1.6.0
+fi
+
+# Build GDAL
+if [ $1 == "gdal" ]; then
+  build_and_push gdal 2.4.0
 fi
