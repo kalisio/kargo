4338c234ab81c53eab583c0d6d1337ed35e9b1ff
Luc Claustres <luc.claustres@orange.fr>
2020-10-21 08:02:38 +0200

    WIP https://github.com/kalisio/kargo/issues/187 (fixed logspout healthcheck)

--- build/logspout/modules.go
+++ build/logspout/modules.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	_ "github.com/kalisio/logspout-slack"
+	_ "github.com/briceburg/logspout-healthcheck"
 	_ "github.com/gliderlabs/logspout/adapters/multiline"
 	_ "github.com/gliderlabs/logspout/adapters/raw"
 	_ "github.com/gliderlabs/logspout/adapters/syslog"
-	_ "github.com/gliderlabs/logspout/healthcheck"
 	_ "github.com/gliderlabs/logspout/httpstream"
 	_ "github.com/gliderlabs/logspout/routesapi"
 	_ "github.com/gliderlabs/logspout/transports/tcp"
