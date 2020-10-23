3f98d2b037235cdd33ed2404ba0f0998bb590065
Christophe Nouguier <christophe.nouguier@free.fr>
2020-10-23 21:37:53 +0200

    feat: Allow to provide any options of the mapcache seeder when using the seed-cache script (close #191)

--- docs/reference/helper-scripts.md
+++ docs/reference/helper-scripts.md
@@ -15,7 +15,7 @@ Clears the cache of a given layer
 ### Example
 
 ```bash
-$./kargo exec clear-cache "osm-bright 9 6"
+$./kargo exec clear-cache osm-bright
 ```
 
 ## seed-cache
@@ -24,14 +24,18 @@ $./kargo exec clear-cache "osm-bright 9 6"
 
 Seeds the cache of a given layer
 
-`usage: seed-cache <layer> <max-level> <nb-threads>`
+`usage: seed-cache <options...>`
 
 Where:
-* `max-level` specifies the maximum level
-* `nb-threads` specifies the number of threads to used when seeding
+* `nb-threads` specifies the number of threads to used when seeding.
+* `layer` specifies the layer to seed.
+* `min-level` specifies the minimum level.
+* `max-level` specifies the maximum level.
+* `extent` specifies the spatial extent, `minx,miny,maxx,maxy` to be taken into account. It must be expressed in the coordinate system of the specified layer.
 
 ### Example
 
 ```bash
-$./kargo exec seed-cache "osm-bright 9 6"
+$./kargo exec seed-cache "6 osm-bright 0 7"  # seed osm-bright over the world
+$./kargo exec seed-cache "6 osm-bright 8 12 -625000,5020000,1250000,6880000"  # seed osm-bright over France
 ```
--- scripts/seed-cache.sh
+++ scripts/seed-cache.sh
@@ -1,33 +1,29 @@
+
 #!/bin/bash
 
 usage() {
-  echo "usage: seed-cache <nb-threads> <layer> <min-level> <max-level> [extent]"
+  echo "usage: seed-cache <options...>"
 }
 
 help() {
-  echo "Seeds the given layer up to the specified level using the specified number of threads"
+  echo "Seeds the given layer using the given options"
   usage
 }
 
 exec() {
-  local NB_THREADS=$1
-  local LAYER=$2
-  local MIN_LEVEL=$3
-  local MAX_LEVEL=$4
-  local EXTENT=$5
-  echo "Seeding $LAYER with $NB_THREADS threads (levels $MIN_LEVEL to $MAX_LEVEL / extent: $EXTENT)"
+  local OPTIONS=$@
+  echo "Seeding with options $OPTIONS"
   local CONFIG_PATH=`pwd`/../configs/mapcache
-  k-worker-foreach -c "docker run -d --rm --network=$DOCKER_NETWORK --user=www-data:www-data --volume=$MAPCACHE_DATA_PATH:/mnt/data --volume=$CONFIG_PATH:/etc/mapcache kalisio/mapcache:1.10 mapcache_seed -c /etc/mapcache/mapcache.xml -t $LAYER -z $MIN_LEVEL,$MAX_LEVEL -n $NB_THREADS -e $EXTENT"
+  k-worker-foreach -c "docker run -d --rm --network=$DOCKER_NETWORK --user=www-data:www-data --volume=$MAPCACHE_DATA_PATH:/mnt/data --volume=$CONFIG_PATH:/etc/mapcache kalisio/mapcache:1.10 mapcache_seed -c /etc/mapcache/mapcache.xml $OPTIONS"
 }
 
-if [ "$#" -ne 4 ] && [ "$#" -ne 5 ]; then
-  echo error: illegal number of arguments
-  usage
-  exit 1
+if [ $# -eq 0 ]; then
+    usage
+    exit 1
 fi
 
 case $1 in
   -h|--help) help;;
-  *) exec "$1" "$2" "$3" "$4" "$5"
+  *) exec "$@"
 esac
 
