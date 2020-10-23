37f73ffe383bbe8f776be5702c6a1ac63144da49
cnouguier <contact@kalisio.com>
2020-10-23 18:23:55 +0000

    wip: Allow to provide a spatial extent when seeding #191 [skip ci]

--- scripts/seed-cache.sh
+++ scripts/seed-cache.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 usage() {
-  echo "usage: seed-cache <layer> <max-level> <nb-threads>"
+  echo "usage: seed-cache <nb-threads> <layer> <min-level> <max-level> [extent]"
 }
 
 help() {
@@ -10,15 +10,17 @@ help() {
 }
 
 exec() {
-  local LAYER=$1
-  local MAX_LEVEL=$2
-  local NB_THREADS=$3
-  echo Seeding $LAYER up to $MAX_LEVEL level with $NB_THREADS threads
+  local NB_THREADS=$1
+  local LAYER=$2
+  local MIN_LEVEL=$3
+  local MAX_LEVEL=$4
+  local EXTENT=$5
+  echo "Seeding $LAYER with $NB_THREADS threads (levels $MIN_LEVEL to $MAX_LEVEL / extent: $EXTENT)"
   local CONFIG_PATH=`pwd`/../configs/mapcache
-  k-worker-foreach -c "docker run -d --rm --network=$DOCKER_NETWORK --user=www-data:www-data --volume=$MAPCACHE_DATA_PATH:/mnt/data --volume=$CONFIG_PATH:/etc/mapcache kalisio/mapcache:1.10 mapcache_seed -c /etc/mapcache/mapcache.xml -t $LAYER -z0,$MAX_LEVEL -n $NB_THREADS"
+  k-worker-foreach -c "docker run -d --rm --network=$DOCKER_NETWORK --user=www-data:www-data --volume=$MAPCACHE_DATA_PATH:/mnt/data --volume=$CONFIG_PATH:/etc/mapcache kalisio/mapcache:1.10 mapcache_seed -c /etc/mapcache/mapcache.xml -t $LAYER -z $MIN_LEVEL,$MAX_LEVEL -n $NB_THREADS -e $EXTENT"
 }
 
-if [ "$#" -ne 3 ]; then
+if [ "$#" -ne 4 ] && [ "$#" -ne 5 ]; then
   echo error: illegal number of arguments
   usage
   exit 1
@@ -26,6 +28,6 @@ fi
 
 case $1 in
   -h|--help) help;;
-  *) exec "$1" "$2" "$3"
+  *) exec "$1" "$2" "$3" "$4" "$5"
 esac
 
