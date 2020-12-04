221da0d1f5819cef52af5aad81eca85c04ddd049
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-12-04 14:17:13 +0000

    Deploy kalisio/kargo to github.com/kalisio/kargo.git:gh-pages

--- service-worker.js
+++ service-worker.js
@@ -27,27 +27,27 @@ self.addEventListener('message', (event) => {
 self.__precacheManifest = [
   {
     "url": "404.html",
-    "revision": "0247e31b2170eede849bf69c55cbf2e9"
+    "revision": "e0808a9e632ea8a9c9526f317c8d9cda"
   },
   {
     "url": "about/contact.html",
-    "revision": "00b0ddf201883d0c96a60069520dcc02"
+    "revision": "151cc34947d6edd289aa921158e93c1f"
   },
   {
     "url": "about/contributing.html",
-    "revision": "eed7d92a67aff61ae6b7e2947feaa4b3"
+    "revision": "0bfb63f7709c9a30dcc3828843964159"
   },
   {
     "url": "about/introduction.html",
-    "revision": "c228a0887cfa34b599aaaf2a0ed907be"
+    "revision": "3808e48c2e0f2fd60010c7a2a2cf8717"
   },
   {
     "url": "about/license.html",
-    "revision": "e3ebc9b2b5825e689ddd964309fee451"
+    "revision": "fced501c3761d51133485a7f6dd9372b"
   },
   {
     "url": "about/roadmap.html",
-    "revision": "7f153c1774e9fff671985ccc7fdfd5b9"
+    "revision": "95ea02b7b7b45a20d7ca74aff559fa04"
   },
   {
     "url": "assets/css/0.styles.4394fa43.css",
@@ -182,60 +182,60 @@ self.__precacheManifest = [
     "revision": "622e6f59b48d091ee14c0fd1c69f0aa5"
   },
   {
-    "url": "assets/js/app.87a7379b.js",
-    "revision": "4c0c4c5a50438587019bb11eedf5061e"
+    "url": "assets/js/app.3a93491d.js",
+    "revision": "563e4ff20279e34457befccb5ee39b2e"
   },
   {
     "url": "guides/advanced-usage.html",
-    "revision": "3ab3095284cda2a6c2f3c005f0691189"
+    "revision": "3f3d868a794e623460c790f0102f6500"
   },
   {
     "url": "guides/getting-started.html",
-    "revision": "bf5a6c9d2b9124b47c5328a4a6b0070a"
+    "revision": "9a1d30148bb3f7748e2432cdfd59b013"
   },
   {
     "url": "guides/understanding-kargo.html",
-    "revision": "1b1222fdc3c4545ebd213463dba87ab9"
+    "revision": "5d7a828aea20ed8a42c64b068e4ac8d3"
   },
   {
     "url": "index.html",
-    "revision": "4c3b5106202b9ef21a6af443a70399f4"
+    "revision": "59ea2046cc8547f7f2f83fdfda8c76a5"
   },
   {
     "url": "reference/cli.html",
-    "revision": "6752658745f1f15d0fcea9c3048bdb9d"
+    "revision": "14b11a82a8909d76fce0f9ac418d37da"
   },
   {
     "url": "reference/environment.html",
-    "revision": "7c6b1794c3b62f9d7f21dd80f7ebdf0b"
+    "revision": "89ae4d772d869885e63f90edcaa2e969"
   },
   {
     "url": "reference/helper-scripts.html",
-    "revision": "061d3a6cc70b9a1ae291214afade0054"
+    "revision": "617779ed4fa11efae893fd7015289883"
   },
   {
     "url": "tips/extracting-osm-data.html",
-    "revision": "6e251a082965ba25d49360a33ac903e8"
+    "revision": "95a6a0af996881ab97292ac442b739b6"
   },
   {
     "url": "tips/handling-long-running-process.html",
-    "revision": "787308e49da5ea60348951ff7697d68c"
+    "revision": "667ae9b2022cfc2d0b0895c3b2119550"
   },
   {
     "url": "tips/using-artillery.html",
-    "revision": "7dc0266fa541f1cb93f1be552cb52da3"
+    "revision": "80b0bf783c814556e8aa2e80dbed584d"
   },
   {
     "url": "tips/using-gdal.html",
-    "revision": "aa2a8956133fe8fe476e8d6ae083c775"
+    "revision": "f850fcbbacf53ed9dcb1fee0caafec25"
   },
   {
     "url": "tips/using-mongodb-atlas.html",
-    "revision": "e7751d84517a1f1eb498051afacf4de6"
+    "revision": "b7b0c5562d9b2ebaf6746e278c068b47"
   },
   {
     "url": "tips/using-private-registry.html",
-    "revision": "251bea95214c52a64473e2a0e34dabd5"
+    "revision": "b05d21725607c6da70b64bdfab3b83d9"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
