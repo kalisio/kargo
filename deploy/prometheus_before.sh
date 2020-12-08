b693cbeb4b7690d7d6e5cb4b1f940d987e86562b
cnouguier <christophe.nouguier@free.fr>
2020-12-08 16:43:18 +0000

    wip: Allow to define the prometheus targets #209

--- deploy/prometheus_before.sh
+++ deploy/prometheus_before.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-for TARGET in $PROMETHEUS_TARGET; do
-yq
-
+cp configs/prometheus/prometheus.config configs/prometheus/prometheus.yml
+for TARGET in $PROMETHEUS_TARGETS; do
+  echo adding $TARGET target
+  yq m -i -a append configs/prometheus/prometheus.yml configs/prometheus/targets/$TARGET.yml
 done
