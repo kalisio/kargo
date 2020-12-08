4c5d27e0d399c0a50f231c732b0740acdf205e5d
Christophe Nouguier <christophe.nouguier@free.fr>
2020-12-08 17:36:20 +0100

    wip: Allow to define the prometheus targets #209 [skip ci]

--- deploy/mongors-exporter_before.sh
+++ deploy/mongors-exporter_before.sh
@@ -2,9 +2,3 @@
 
 envsubst '${MONGORS_NAME}' < deploy/mongors-exporter.yml.tpl > deploy/mongors-exporter.yml
 
-# add prometheus scrape job
-cat <<EOT >> configs/prometheus/prometheus.yml
-  - job_name: 'mongodb'
-    static_configs:
-      - targets: [ '${MONGORS_NAME}0-exporter:9216', '${MONGORS_NAME}1-exporter:9216', '${MONGORS_NAME}2-exporter:9216' ]
-EOT
