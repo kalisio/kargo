f979d374c0fb0fbab71a2859148d56c49257be17
Christophe Nouguier <christophe.nouguier@free.fr>
2021-03-16 21:28:54 +0100

    wip: Provide basic scripts to create/backup/restore Postgis/Mariadb databases #230 [skip ci]

--- scripts/slack.sh
+++ scripts/slack.sh
@@ -8,7 +8,7 @@ source ${KARGO_SCRIPTS_PATH}/log.sh
 notify_slack() {
   local MESSAGE=$1
   local STYLE=${2:-default}
-  if [[ -z "${SLACK_WEBHOOK_URL-}" ]]; then
+  if [[ -n "${SLACK_WEBHOOK_URL-}" ]]; then
     local PAYLOAD="{ \"attachments\": [ { \"color\": \"${STYLE}\", \"title\": \"${SUBDOMAIN}\", \"mrkdwn_in\": [ \"text\" ], \"text\": \"${MESSAGE}\" } ]"
     curl -X POST -H "Content-type: application/json" --data "${PAYLOAD}" ${SLACK_WEBHOOK_URL} > /dev/null
   else
