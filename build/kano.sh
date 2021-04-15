08c8ba147ac7327d7adf5428922f159b0a5dda78
Robin Bourianes <robin.bourianes@frizko.com>
2021-04-15 16:07:28 +0200

    feat: kano build can now push to a private registry. [skip build][skip doc]

--- build/kano.sh
+++ build/kano.sh
@@ -3,5 +3,10 @@ cd kano
 # Create the archive to be merged 
 tar cvf kano.tgz -C ../../configs/kano .
 # Build the image
-docker build --pull --force-rm --build-arg TAG=$KANO_TAG --build-arg SUBDOMAIN --build-arg NODE_APP_INSTANCE --build-arg NODE_ENV -f dockerfile -t localhost:5000/kano:$KANO_TAG .
-docker push localhost:5000/kano:$KANO_TAG
+docker build --pull --force-rm --build-arg TAG=$KANO_TAG --build-arg SUBDOMAIN --build-arg NODE_APP_INSTANCE --build-arg NODE_ENV -f dockerfile -t $KANO_IMAGE:$KANO_TAG .
+if [ -n "$REGISTRY_USERNAME" ]; then
+    # Extract registry url from image
+    REGISTRY_URL=$(echo $KANO_IMAGE | awk -F '/' '{ print $1 }')
+    docker login "$REGISTRY_URL" -u "$REGISTRY_USERNAME" -p "$REGISTRY_PASSWORD"
+fi
+docker push $KANO_IMAGE:$KANO_TAG
