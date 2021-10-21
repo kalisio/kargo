fd9acc2a2fbf0592358f8472581682873aa64995
cnouguier <christophe.nouguier@free.fr>
2021-10-21 14:31:14 +0200

    wip: Allow to define additionnal S3 information in order to use alternative S3 providers than AWS #261

--- build/express-gateway/plugins/storage.js
+++ build/express-gateway/plugins/storage.js
@@ -3,7 +3,32 @@ const _ = require('lodash');
 const aws = require('aws-sdk');
 
 let storageProxies = {}
-let defaultStorageProxy
+
+function parseParams (params, providers) {
+  let tokens = _.split(params, '/')
+  let provider = _.nth(tokens, 0)
+  if (provider) {
+    let bucket, keyIndex
+    // Check wehther th
+    if (!_.includes(_.keys(providers), provider)) {
+      bucket = provider
+      keyIndex = 1
+      provider = _.findKey(providers, { default: true })
+    }
+    if (provider) {
+      if (!bucket) {
+        bucket = _.nth(tokens, 1)
+        keyIndex = 2
+      }
+      if (bucket && keyIndex) {
+        console.log(_.slice(tokens, keyIndex))
+        let key = _.join(_.slice(tokens, keyIndex), '/')
+        return { provider, bucket, key }
+      }
+    }
+  }
+  return null
+}
 
 module.exports = {
   version: '1.2.0',
@@ -15,46 +40,17 @@ module.exports = {
         if (accessKeyId && secretAccessKey) {
           logger.info('Creating storage proxy ' + provider)
           storageProxies[provider] = new aws.S3(_.omit(options, 'default'))
-          if (options.default) {
-            logger.info('Setting default storage proxy to ' + provider)
-            defaultStorageProxy = storageProxies[provider]
-          }
         } else {
           logger.info('Missing accessKeyId and/or secretAccessKey for provider ' + provider)
         }
       }
-      // Specific provider route
-      app.get(pluginContext.settings.endpointName + '/:provider/:bucket/*', (req, res, next) => {
-        const provider = req.params.provider;
-        const storageProxy = storageProxies[provider];
-        if (!storageProxy) next()
-        storageProxy.getObject({
-          Bucket: req.params.bucket,
-          Key: req.params[0],
-          Range: req.headers.range // Forward range requests
-        })
-        .on('httpHeaders', (statusCode, headers) => { 
-          // Avoid catching event raised by connection initialization
-          // https://stackoverflow.com/questions/35782434/streaming-file-from-s3-with-express-including-information-on-length-and-filetype
-          if (headers['content-length']) {
-            res.status(statusCode);
-            res.set(headers);
-          } else logger.debug('Invalid response with status ' + statusCode)
-        })
-        .createReadStream()
-        .on('error', (err) => {
-          logger.debug(err);
-          return res.status(404).send(err);
-        })
-        .pipe(res);
-      });
-      // Default provider route
-      app.get(pluginContext.settings.endpointName + '/:bucket/*', (req, res) => {
-        logger.info('falling back to default provider ' + defaultStorageProxy)
-        if (defaultStorageProxy) {
-          defaultStorageProxy.getObject({
-            Bucket: req.params.bucket,
-            Key: req.params[0],
+      app.get(pluginContext.settings.endpointName + '/*', (req, res) => {
+        const params = parseParams(req.params[0], pluginContext.settings.providers)
+        if (params.provider && params.bucket && params.key) {
+          const storageProxy = storageProxies[params.provider];
+          storageProxy.getObject({
+            Bucket: params.bucket,
+            Key: params.key,
             Range: req.headers.range // Forward range requests
           })
           .on('httpHeaders', (statusCode, headers) => { 
