b0ca307bb656360873e5834308dbb3fbc6c0f980
cnouguier <ch.nouguier@gmail.com>
2023-04-04 18:42:51 +0200

    wip: Enhance ci to build images with latest or current app version tag #314 [build gdal]

--- .travis.build.sh
+++ .travis.build.sh
@@ -1,71 +1,63 @@
 #!/bin/bash
+set -euxo pipefail
 
-build_and_push()
-{
-  TOOL=$1
-  VERSION=$2
-  CONTEXT="build/$TOOL"
-  DOCKERFILE_OPT="-f build/$TOOL/dockerfile"
-  if [ $# -ge 3 ]; then
-    CONTEXT=$3
+COMMIT_MESSAGE=$1
+APP=`echo ^$COMMIT_MESSAGE | cut -d' ' -f2`
+
+VERSION=
+CONTEXT="build/$APP"
+DOCKERFILE_OPT="-f build/$APP/dockerfile"
+
+case $APP in
+  express-gateway)
+    VERSION=1.16.9
+    ;;
+  mapcache)
+    VERSION=1.10
+    ;;
+  mapserver)
+    VERSION=7.4
+    ;;
+  maputnik)
+    VERSION=1.6.1
+    ;;
+  tileserver-gl)
+    VERSION=v3.1.1
+    ;;
+  gdal)
+    VERSION=3.4.0
+    ;;
+  monggodb-exporter)
+    VERSION=v0.11.2
+    CONTEXT=https://github.com/percona/mongodb_exporter.git#v0.11.2
     DOCKERFILE_OPT=""
-  fi
+    ;;
+  kaptain)
+    VERSION=latest
+    ;;
+  k8s-toolbox
+    VERSION=latest
+    ;;  
+  *)
+    echo -n "unknown APP"
+    ;;
+esac
 
+TAG=`echo ^$COMMIT_MESSAGE | cut -d' ' -f3`
+if [ -z $TAG ]; then 
+  TAG=latest
+else
+  TAG=$VERSION
+fi
+
+if [ ! z $VERSION ]; then
   docker login -u="$DOCKER_USER" -p="$DOCKER_PASSWORD"
-  echo "Building $TOOL:$VERSION from $CONTEXT"
-  docker build --force-rm --build-arg VERSION=$VERSION $DOCKERFILE_OPT -t kalisio/$TOOL:$VERSION $CONTEXT
+  echo "Building $APP:$VERSION from $CONTEXT"
+  docker build --force-rm --build-arg VERSION=$VERSION $DOCKERFILE_OPT -t kalisio/$APP:$TAG $CONTEXT
   RESULT_CODE=$?
   if [ $RESULT_CODE -ne 0 ]; then
-    echo "$TOOL generation failed [error: $RESULT_CODE]"
+    echo "$APP generation failed [error: $RESULT_CODE]"
     exit 1
   fi
-  docker push kalisio/$TOOL:$VERSION
-}
-
-# Build express-gateway
-if [ $1 == "express-gateway" ]; then
-  build_and_push express-gateway 1.16.9
-fi
-
-# Build MapCache
-if [ $1 == "mapcache" ]; then 
-  build_and_push mapcache 1.10
-fi
-
-# Build MapCache
-if [ $1 == "mapserver" ]; then 
-  build_and_push mapserver 7.4
-fi
-
-# Build Maputnik
-if [ $1 == "maputnik" ]; then
-  build_and_push maputnik 1.6.1
-fi
-
-# Build Tileserver-GL
-if [ $1 == "tileserver-gl" ]; then
-  build_and_push tileserver-gl v3.1.1
-fi
-
-# Build Artillery
-if [ $1 == "artillery" ]; then
-  build_and_push artillery 1.6.0
-fi
-
-# Build GDAL
-if [ $1 == "gdal" ]; then
-  build_and_push gdal 3.4.0
-fi
-
-# Build mongodb-exporter
-if [ $1 == "mongodb-exporter" ]; then
-  build_and_push mongodb-exporter v0.11.2 https://github.com/percona/mongodb_exporter.git#v0.11.2
-fi
-
-if [ $1 == "kaptain" ]; then
-  build_and_push kaptain latest
-fi
-
-if [ $1 == "k8s-toolbox" ]; then
-  build_and_push k8s-toolbox latest
+  docker push kalisio/$APP:$TAG
 fi
