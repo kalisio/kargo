168f5bd0670b9ac42512af8f2f8e3db78072dd88
Robin Bourianes <robin.bourianes@gmail.com>
2023-11-09 11:34:37 +0100

    chore: release-dev-chart.sh script can now release all dev charts if parameter is omitted

--- scripts/release-dev-chart.sh
+++ scripts/release-dev-chart.sh
@@ -2,36 +2,48 @@
 set -euo pipefail
 # set -x
 
-CHART=${1}
+SINGLE_CHART=${1:-}
 REMOTE="oci://harbor.portal.kalisio.com/kalisio/helm"
 LATEST_VERSION=0.0.0-dev
 
-THIS_FILE=$(readlink -f "$BASH_SOURCE")
+THIS_FILE=$(readlink -f "${BASH_SOURCE[0]}")
 THIS_PATH=$(dirname "$THIS_FILE")
 KARGO_PATH=$(dirname "$THIS_PATH")
 TMP_PATH=$(mktemp -d -p "${XDG_RUNTIME_DIR:-}" kargo_charts.XXXXXX)
 
 # get in root kargo folder
-cd $KARGO_PATH
+cd "$KARGO_PATH"
 
-# make sure chart exists
-if [ ! -d "charts/$CHART" ]; then
-    echo "$0: chart $CHART does not exist"
-    exit 1
+if [ -z "$SINGLE_CHART" ]; then
+    # empty SINGLE_CHART => release all charts
+    for CHART in charts/*/; do
+        helm dependencies update "$CHART"
+        helm lint "$CHART"
+        helm package "$CHART" --destination "$TMP_PATH" --version "$LATEST_VERSION"
+    done
+else
+    # single chart release
+    # make sure chart exists
+    if [ ! -d charts/"$SINGLE_CHART" ]; then
+        echo "$0: chart $SINGLE_CHART does not exist"
+        exit 1
+    fi
+
+    # package chart
+    helm dependencies update charts/"$SINGLE_CHART"
+    helm lint charts/"$SINGLE_CHART"
+    helm package charts/"$SINGLE_CHART" --destination "$TMP_PATH" --version $LATEST_VERSION
 fi
 
-# package chart
-helm dependencies update charts/$CHART
-helm lint charts/$CHART
-helm package charts/$CHART --destination $TMP_PATH --version $LATEST_VERSION
-
 # push on oci registry
-helm push $TMP_PATH/$CHART-$LATEST_VERSION.tgz $REMOTE
+for CHART in "$TMP_PATH"/*.tgz; do
+    helm push "$CHART" "$REMOTE"
+done
 
 # and on s3 backup storage (merge index.yaml before pushing)
-rclone copy kalisio_charts:index.yaml $TMP_PATH
-helm repo index $TMP_PATH --merge $TMP_PATH/index.yaml
-rclone copy $TMP_PATH kalisio_charts:
+rclone copy kalisio_charts:index.yaml "$TMP_PATH"
+helm repo index "$TMP_PATH" --merge "$TMP_PATH"/index.yaml
+rclone copy "$TMP_PATH" kalisio_charts:
 
 # cleanup
-rm -fR $TMP_PATH
+rm -fR "$TMP_PATH"
