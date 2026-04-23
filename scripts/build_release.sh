4eee371d18a7adc74ea93c5041ac8f9bcd91c982
hndiaye9854 <hamidoundiaye9853@gmail.com>
2026-04-23 14:01:17 +0200

    Test: Change the value of the version of maputnik to resolve tag problem

--- scripts/build_release.sh
+++ scripts/build_release.sh
@@ -70,8 +70,8 @@ end_group "Setup rclone config"
 
 ## Release charts
 ##
-FORCE_DEV="${FORCE_DEV:-false}"*
-echo "-> FORCE_DEV = '${FORCE_DEV}'" 
+FORCE_DEV="${FORCE_DEV:-false}"
+git 
 RCLONE_REMOTE="kalisio_charts"
 
 # Set git identity for tag creation — CI mode only
